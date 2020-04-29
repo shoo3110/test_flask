594d1f61eeb294829f8b8efda0efb0fdee9cd8c9
saitoshoichi <shoot319@gmail.com>
2020-04-29 15:36:24 +0900

    third

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
+comments=[]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    message = 'sample_string'
+    return render_template('index.html',message=message)
 
-@app.route('/submit',methods=['POST'])
+@app.route('/submit',methods=['Get','POST'])
 def submit():
     return 'You entered:{}'.format(request['memo'])
